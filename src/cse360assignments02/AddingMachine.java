301a41f63fe049d65b726c21b224bbd8c1506df0
Nicholas Morello <nickvmorello@gmail.com>
2020-10-05 21:55:25 -0700

    Initial commit

---
+++ src/cse360assignments02/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assignments02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Constructor
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * getTotal()
+     * return the current total
+     * @return
+     */
+    public int getTotal () {
+
+        return 0;
+    }
+
+    /**
+     * add()
+     *  add the parameter to the total variable
+     * @param value
+     */
+    public void add (int value) {
+
+    }
+
+    /**
+     * substract()
+     * subtract the parameter from the total variable
+     * @param value
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * toString()
+     * @return
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * clear()
+     * should clear your "memory"
+     */
+    public void clear() {
+    }
+}
---
+++ src/cse360assignments02/main.java
@@ -0,0 +1,9 @@
+package cse360assignments02;
+
+public class main {
+    public static void main(String[] args) {
+        // write your code here
+        AddingMachine myCalc = new AddingMachine();
+        myCalc.add(4);
+    }
+}
